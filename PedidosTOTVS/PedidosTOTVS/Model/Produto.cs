751030abc50b9d23f659cfbd02388205e958371c
Unknown <wyrmnax@gmail.com>
2018-09-01 18:38:58 -0300

    Base Models

---
+++ PedidosTOTVS/PedidosTOTVS/Model/Produto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PedidosTOTVS.Model
+{
+    public class Produto
+    {
+        public int ID { get; set; }
+        public string Descricao { get; set; }
+        public float ValorIndividual { get; set; }
+        public int Quantidade { get; set; }
+    }
+}
